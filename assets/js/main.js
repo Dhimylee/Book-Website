9e1efeb8c49f426a6222643002191fb0ae5f435e
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-05-21 21:36:43 -0300

    Feat: implementando evento de clique no botao de login

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,24 @@ if(searchClose) {
         searchContent.classList.remove('show-search')
     })
 }
+
+/*================ LOGIN ================ */
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close')
+      loginContent = document.getElementById('login-content')
+
+/* ====== LOGIN SHOW ====== */
+// Validate if constant exists
+if(loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
+
+/* ====== LOGIN HIDDEN ====== */
+// Validate if constant exists
+if(loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login')
+    })
+}
